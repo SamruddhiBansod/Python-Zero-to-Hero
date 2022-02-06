a933fd3117babbae61349b8ad7b62615d44dc9ad
Samruddhi27 <84733929+SamruddhiBansod@users.noreply.github.com>
2022-02-06 16:55:00 +0530

    Assignment Day5 submitted

---
+++ Day5.py
@@ -0,0 +1,7 @@
+mylist = list(range(1, 50))
+
+result3 = list(filter(lambda x: (x % 3 == 0), mylist))
+result5 = list(filter(lambda x: (x % 5 == 0), mylist))
+
+print("This number only divisible by 3 in 1-50 numbers : \n", result3)
+print("This number only divisible by 5 in 1-100 number : ", result5)
